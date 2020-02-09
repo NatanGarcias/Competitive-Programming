51242fa3e67a7eb5684cea060bb2036add59cb54
NatanGarcias <natans.garcias@gmail.com>
2020-02-09 15:10:59 -0300

    Limpando e Organizando as pastas

--- III Maratona Baiana de Programação - 2019/teste.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    cout << 100000 << " " << 2 << endl;
-    for(int i=1;i<=10000;i++){
-        cout << i << " " << i << endl;
-    }
-}
---
+++ SPOJ/Life,theUniverse,andEverything.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define optimize ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+#define ll long long
+#define ld long double 
+
+#define endl "\n"
+
+#define pii pair<int,int>
+#define fi first 
+#define se second 
+
+#define INF 0x7fffffff
+#define INFLL 0x7fffffffffffffff
+
+int main(){
+
+    int a;
+
+    while(cin >> a){
+        if(a == 42)
+            return 0;
+
+        cout << a << endl;
+    }
+    return 0;
+}
